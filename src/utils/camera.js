60ff238505f34cb355cdc5685b52f340836d02e1
Chase <112102369+chasecraft88@users.noreply.github.com>
2023-10-31 23:03:23 +0000

    Update camera.js

--- src/utils/camera.js
+++ src/utils/camera.js
@@ -1,26 +1,24 @@
 import * as THREE from 'three';
 
-export const updateCamera = (camera, player, cameraAngel, xDir, zDir) => {
-  if (cameraAngel === 1) {
+export const updateCamera = (camera, player, cameraAngle, xDir, zDir) => {
+  if (cameraAngle === 1) {
     camera.position.x = player.position.x;
     camera.position.y = player.position.y + 0.5;
     camera.position.z = player.position.z;
     camera.lookAt(player.position.x + xDir, 0.75, player.position.z + zDir);
   }
 
-  if (cameraAngel === 2) {
+  if (cameraAngle === 2) {
     camera.position.x = player.position.x;
     camera.position.y = 15;
     camera.position.z = player.position.z;
     camera.lookAt(player.position);
   }
 
-  if (cameraAngel === 3) {
-    let relativeCameraOffset = new THREE.Vector3(0, 4, 3);
-    let cameraOffset = relativeCameraOffset.applyMatrix4(player.matrixWorld);
-    camera.position.x = cameraOffset.x;
-    camera.position.y = cameraOffset.y;
-    camera.position.z = cameraOffset.z;
+  if (cameraAngle === 3) {
+    const relativeCameraOffset = new THREE.Vector3(0, 4, 3);
+    const cameraOffset = relativeCameraOffset.applyMatrix4(player.matrixWorld);
+    camera.position.copy(cameraOffset);
     camera.lookAt(player.position);
   }
 };
